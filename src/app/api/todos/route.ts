e75be591584ca41546518a5ddd505229bae7c13d
Francisco Lino <franksuriel_7@hotmail.com>
2024-03-24 09:45:03 -0600

    ADD POST todo

--- src/app/api/todos/route.ts
+++ src/app/api/todos/route.ts
@@ -1,5 +1,6 @@
 import prisma from "@/lib/prisma";
 import { NextResponse, NextRequest } from "next/server";
+import * as yup from "yup";
 
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
@@ -20,4 +21,21 @@ export async function GET(request: Request) {
     skip: skip,
   });
   return NextResponse.json(todos);
+}
+
+const postSchema = yup.object({
+  description: yup.string().required(),
+  complete: yup.boolean().optional().default(false),
+});
+
+export async function POST(request: Request) {
+  try {
+    const { complete, description } = await postSchema.validate(
+      await request.json()
+    );
+    const todo = await prisma.todo.create({ data: { complete, description } });
+    return NextResponse.json(todo);
+  } catch (error) {
+    return NextResponse.json(error, { status: 404 });
+  }
 }
