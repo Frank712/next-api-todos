c86d1234f23e598bcf1460d27e86766a6342dc1b
Francisco Lino <franksuriel_7@hotmail.com>
2024-03-28 15:46:50 -0600

    ADD login logout

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -14,6 +14,7 @@ import {
 } from "react-icons/io5";
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import { getServerSession } from "next-auth";
+import { LogoutButton } from "./LogoutButton";
 
 const menuItems = [
   {
@@ -95,10 +96,7 @@ export const Sidebar = async () => {
       </div>
 
       <div className="px-6 -mx-6 pt-4 flex justify-between items-center border-t">
-        <button className="px-4 py-3 flex items-center space-x-4 rounded-md text-gray-600 group">
-          <CiLogout />
-          <span className="group-hover:text-gray-700">Logout</span>
-        </button>
+        <LogoutButton />
       </div>
     </aside>
   );
