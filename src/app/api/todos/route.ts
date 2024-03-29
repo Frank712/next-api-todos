fe2d454a3c3f46b21bba8e6232e3229a68cfd0e4
Francisco Lino <franksuriel_7@hotmail.com>
2024-03-29 11:26:15 -0600

    UPDATE add validations

--- src/app/api/todos/route.ts
+++ src/app/api/todos/route.ts
@@ -1,3 +1,4 @@
+import { getUserSessionServer } from "@/auth/actions/auth-actions";
 import prisma from "@/lib/prisma";
 import { NextResponse, NextRequest } from "next/server";
 import * as yup from "yup";
@@ -29,11 +30,17 @@ const postSchema = yup.object({
 });
 
 export async function POST(request: Request) {
+  const user = await getUserSessionServer();
+  if (!user) {
+    return NextResponse.json("No autorizado", { status: 401 });
+  }
   try {
     const { complete, description } = await postSchema.validate(
       await request.json()
     );
-    const todo = await prisma.todo.create({ data: { complete, description } });
+    const todo = await prisma.todo.create({
+      data: { complete, description, userId: user.id },
+    });
     return NextResponse.json(todo);
   } catch (error) {
     return NextResponse.json(error, { status: 404 });
@@ -41,8 +48,14 @@ export async function POST(request: Request) {
 }
 
 export async function DELETE(request: Request) {
+  const user = await getUserSessionServer();
+  if (!user) {
+    return NextResponse.json("No autorizado", { status: 401 });
+  }
   try {
-    await prisma.todo.deleteMany({ where: { complete: true } });
+    await prisma.todo.deleteMany({
+      where: { complete: true, userId: user.id },
+    });
     return NextResponse.json({ message: "Delete successfully" });
   } catch (error) {
     return NextResponse.json(error, { status: 404 });
--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -54,7 +54,7 @@ export const Sidebar = async () => {
   const imageUrl =
     session?.user?.image ||
     "https://cdn-icons-png.flaticon.com/512/3607/3607444.png";
-  const userName = session?.user?.name || "Inicie sesión";
+  const userName = session?.user?.name || "Sin nombre";
   const roles = session?.user?.roles || ["user"];
   return (
     <aside className="ml-[-100%] fixed z-10 top-0 pb-3 px-6 w-full flex flex-col justify-between h-screen border-r bg-white transition duration-300 md:w-4/12 lg:ml-0 lg:w-[25%] xl:w-[20%] 2xl:w-[15%]">
